3398fe67949e9f64bc6d788ed0a991d94d92cbfe
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2020-03-10 15:06:45 -0400

    Update layers to PyTorch style

--- simple_fhvae.py
+++ simple_fhvae.py
@@ -6,11 +6,9 @@ from torch.nn.init import xavier_uniform_
 
 import costs
 
-loss = nn.CrossEntropyLoss()
-
 
 class SimpleFHVAE(nn.Module):
-    def __init__(self, xin, xout, y, n, nmu2):
+    def __init__(self, xin, xout, mu_idx, n, nmu2):
         super().__init__()
 
         # encoder/decoder arch
@@ -21,7 +19,7 @@ class SimpleFHVAE(nn.Module):
         # observed vars
         self.xin = xin
         self.xout = xout
-        self.y = y
+        self.mu_idx = mu_idx
         self.n = n
         self.nmu2 = nmu2
 
@@ -38,7 +36,7 @@ class SimpleFHVAE(nn.Module):
         ) = self.net(
             self.xin,
             self.xout,
-            self.y,
+            self.mu_idx,
             self.nmu2,
             self.z1_hus,
             self.z1_dim,
@@ -52,30 +50,9 @@ class SimpleFHVAE(nn.Module):
         self.pz2 = [self.mu2, np.log(0.0 ** 2).astype(np.float32)]
         self.pmu2 = [0.0, np.log(1.0 ** 2).astype(np.float32)]
 
-        # variational lower bound
-        self.log_pmu2 = torch.sum(
-            log_gauss(self.mu2, self.pmu2[0], self.pmu2[1]), dim=1
-        )
-        self.neg_kld_z2 = -1 * torch.sum(
-            kld(self.qz2_x[0], self.qz2_x[1], self.pz2[0], self.pz2[1]), dim=1
-        )
-        self.neg_kld_z1 = -1 * torch.sum(
-            kld(self.qz1_x[0], self.qz1_x[1], self.pz1[0], self.pz1[1]), dim=1
-        )
-        self.log_px_z = torch.sum(
-            log_gauss(xout, self.px_z[0], self.px_z[1]), dim=(1, 2)
-        )
-        self.lb = self.log_px_z + self.neg_kld_z1 + self.neg_kld_z2 + self.log_pmu2 / n
-
-        # discriminative loss
-        logits = torch.unsqueeze(self.qz2_x[0], 1) - torch.unsqueeze(self.mu2_table, 0)
-        logits = -1 * torch.pow(logits, 2) / (2 * torch.exp(self.pz2[1]))
-        logits = torch.sum(logits, dim=-1)
-        self.log_qy = loss(input=logits, target=y)
-
-    def net(self, xin, xout, y, nmu2, z1_hus, z1_dim, z2_hus, z2_dim, x_hus):
+    def net(self, xin, xout, mu_idx, nmu2, z1_hus, z1_dim, z2_hus, z2_dim, x_hus):
         """Initialize the network"""
-        self.mu2_table, self.mu2 = mu2_lookup(self.y, z2_dim, self.nmu2)
+        self.mu2_table, self.mu2 = mu2_lookup(self.mu_idx, z2_dim, self.nmu2)
 
         z2_pre_out = z2_pre_encoder(self.xin, z2_hus)
         z2_mu, z2_logvar, z2_sample = gauss_layer(z2_preout, z2_dim)
@@ -93,7 +70,6 @@ class SimpleFHVAE(nn.Module):
         x_sample = torch.reshape(x_sample, (-1, T, F))
         # p(x|z_1, z_2)
         px_z = [x_mu, x_logvar]
-
         return mu2_table, mu2, qz2_x, z2_sample, qz1_x, z1_sample, px_z, x_sample
 
     def __str__(self):
@@ -106,7 +82,7 @@ class SimpleFHVAE(nn.Module):
         msg += "\n  Observed Variables:"
         msg += "\n    xin: %s" % self.xin
         msg += "\n    xout: %s" % self.xout
-        msg += "\n    y: %s" % self.y
+        msg += "\n    mu_idx: %s" % self.mu_idx
         msg += "\n    n: %s" % self.n
         msg += "\n  Encoder/Decoder Architectures:"
         msg += "\n    z1 encoder:"
@@ -138,89 +114,138 @@ class SimpleFHVAE(nn.Module):
             msg += "\n    %s, %s" % (param.name, param.size())
         return msg
 
-    def forward(self, x):
+    def forward(self, x, mu_idx):
+        self.mu2_table, self.mu2 = mu2_lookup()
         pass
 
 
-def mu2_lookup(y, z2_dim, nmu2, init_std=1.0):
+# alpha/discriminative weight of 10 was found to produce best results
+def loss_function(model, target, alpha=10.0):
+    """
+    Discriminative segment variational lower bound
+    Segment variational lower bound plus the (weighted) discriminative objective
+    """
+    loss = nn.CrossEntropyLoss()
+
+    # variational lower bound
+    self.log_pmu2 = torch.sum(log_gauss(self.mu2, self.pmu2[0], self.pmu2[1]), dim=1)
+    self.neg_kld_z2 = -1 * torch.sum(
+        kld(self.qz2_x[0], self.qz2_x[1], self.pz2[0], self.pz2[1]), dim=1
+    )
+    self.neg_kld_z1 = -1 * torch.sum(
+        kld(self.qz1_x[0], self.qz1_x[1], self.pz1[0], self.pz1[1]), dim=1
+    )
+    self.log_px_z = torch.sum(log_gauss(xout, self.px_z[0], self.px_z[1]), dim=(1, 2))
+    lower_bound = self.log_px_z + self.neg_kld_z1 + self.neg_kld_z2 + self.log_pmu2 / n
+
+    # discriminative loss
+    logits = torch.unsqueeze(self.qz2_x[0], 1) - torch.unsqueeze(self.mu2_table, 0)
+    logits = -1 * torch.pow(logits, 2) / (2 * torch.exp(self.pz2[1]))
+    logits = torch.sum(logits, dim=-1)
+    log_qy = loss(input=logits, target=target)
+
+    return -1 * torch.mean(lower_bound + alpha * log_qy)
+
+
+def mu2_lookup(mu_idx, z2_dim, nmu2, init_std=1.0):
     """
     Mu2 posterior mean lookup table
     Args:
-        y(torch.Tensor): int tensor of shape (bs,). Index for mu2_table
+        mu_idx(torch.Tensor): int tensor of shape (bs,). Index for mu2_table
         z2_dim(int): z2 dimension
         nmu2(int): lookup table size
     """
     mu2_table = torch.empty([nmu2, z2_dim], requires_grad=True).normal_(
         mean=4, std=init_std
     )
-    mu2 = torch.gather(mu2_table, y)
+    mu2 = torch.gather(mu2_table, mu_idx)
     return mu2_table, mu2
 
 
-def z1_pre_encoder(x, z2, hus=[1024, 1024]):
+class LatentSegPreEncoder(nn.Module):
     """
     Pre-stochastic layer encoder for z1 (latent segment variable)
     Args:
         x(torch.Tensor): tensor of shape (bs, T, F)
-        z2(torch.Tensor): tensor of shape (bs, D1)
+        lat_seq(torch.Tensor): latent sequence variable (z2)
         hus(list): list of numbers of FC layer hidden units
     Return:
         out(torch.Tensor): last FC layer output
     """
-    T, F = list(x.size())[1:]
-    x = torch.reshape(x, (-1, T * F))
-    out = torch.cat([x, z2], dim=-1)
-    for i, hu in enumerate(hus):
-        out = nn.Linear(np.prod(out.shape[1:]), hu)(out)
-        out = F.relu(out)
-    return out
+
+    def __init__(self, hus=[1024, 1024]):
+        super().__init()
+        self.hus = hus
+
+    def forward(self, x, lat_seq):
+        x = torch.reshape(x, (-1,))
+        out = torch.cat([x, lat_seq])
+        for hu in self.hus:
+            out = nn.Linear(np.prod(out.shape[1:]), hu)(out)
+            out = F.relu(out)
+        return out
 
 
-def z2_pre_encoder(x, hus=[1024, 1024]):
+class LatentSeqPreEncoder(nn.Module):
     """
     Pre-stochastic layer encoder for z2 (latent sequence variable)
     Args:
-        x(torch.Tensor): tensor of shape (bs, T,F)
         hus(list): list of numbers of LSTM layer hidden units
     Return:
         out(torch.Tensor): concatenation of hidden states of all LSTM layers
     """
-    T, F = list(x.size())[1:]
-    out = torch.reshape(x, (-1, T * F))
-    for i, hu in enumerate(hus):
-        out = nn.Linear(np.prod(out.shape[1:]), hu)(out)
-        out = F.relu(out)
-    return out
+
+    def __init__(self, hus=[1024, 1024]):
+        super().__init()
+        self.hus = hus
+
+    def forward(self, x):
+        out = torch.reshape(x, (-1,))
+        for hu in self.hus:
+            out = nn.Linear(np.prod(out.shape[1:]), hu)(out)
+            out = F.relu(out)
+        return out
 
 
-def gauss_layer(inp, dim):
+class GaussianLayer(nn.Module):
     """
     Gaussian layer
     Args:
-        inp(torch.Tensor): input to Gaussian layer
+        input_layer(torch.Tensor): input layer
         dim(int): dimension of output latent variables
     """
-    mu = nn.Linear(np.prod(inp.shape[1:]), dim)(inp)
-    logvar = nn.Linear(np.prod(inp.shape[1:]), dim)(inp)
-    std = torch.exp(0.5 * logvar)
-    eps = torch.randn_like(std)
-    return mu, logvar, mu + eps * std
+
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, input_layer):
+        mu = nn.Linear(np.prod(input_layer.shape[1:]), self.dim)(input_layer)
+        logvar = nn.Linear(np.prod(input_layer.shape[1:]), self.dim)(input_layer)
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu, logvar, mu + eps * std
 
 
-def pre_decoder(z1, z2, hus=[1024, 1024]):
+class PreDecoder(nn.Module):
     """
     Pre-stochastic layer decoder
     Args:
-        z1(torch.Tensor)
-        z2(torch.Tensor)
-        x(torch.Tensor): tensor of shape (bs, T, F). Only shape is used
-        hus(list)
+        lat_seg(torch.Tensor): latent segment Tensor (z1)
+        lat_seq(torch.Tensor): latent sequence Tensor (z2)
+        hus(list): list of hidden units per fully-connected layer
     """
-    out = torch.cat([z1, z2], dim=-1)
-    for i, hu in enumerate(hus):
-        out = nn.Linear(np.prod(out.shape[1:]), hu)(out)
-        out = F.relu(out)
-    return out
+
+    def __init__(self, hus=[1024, 1024]):
+        super().__init__()
+        self.hus = hus
+
+    def forward(self, lat_seg, let_seq):
+        out = torch.cat([lat_seg, lat_seq])
+        for hu in self.hus:
+            out = nn.Linear(np.prod(out.shape[1:]), hu)(out)
+            out = F.relu(out)
+        return out
 
 
 def log_gauss(x, mu=0.0, logvar=0.0):
