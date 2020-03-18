e285df47178a298fd5d82c243dcf6515dcdd47d4
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2020-03-17 21:09:30 -0400

    Refactor dataset preprocessing

--- preprocess_librispeech.py
+++ preprocess_librispeech.py
@@ -3,64 +3,99 @@ import argparse
 from .utils import maybe_makedir
 
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("librispeech_dir", type=str, help="LibriSpeech raw data directory")
-parser.add_argument(
-    "--ftype", type=str, default="fbank", choices=["fbank", "spec"], help="feature type"
-)
-parser.add_argument(
-    "--out_dir",
-    type=str,
-    default="./datasets/librispeech_np_fbank",
-    help="output data directory",
-)
-parser.add_argument(
-    "--train_list",
-    type=str,
-    nargs="*",
-    default=["train-clean-100"],
-    help="train sets to include {train-clean-100, train-clean-360, train-other-500}",
-)
-parser.add_argument(
-    "--dev_list",
-    type=str,
-    nargs="*",
-    default=["dev-clean", "dev-other"],
-    help="dev sets to include {dev-clean, dev-other}",
-)
-parser.add_argument(
-    "--test_list",
-    type=str,
-    nargs="*",
-    default=["test-clean", "dev-other"],
-    help="test sets to include {test-clean, test-other}",
-)
-args = parser.parse_args()
-print(args)
-
-
 # dump wav scp
-def find_audios(d):
+def find_audios(dir: str) -> list:
+    """Find .flac files in the given directory"""
     uid_path = []
-    for root, _, fnames in sorted(os.walk(d)):
+    for root, _, fnames in sorted(os.walk(dir)):
         for fname in fnames:
             if fname.endswith(".flac") or fname.endswith(".FLAC"):
                 uid_path.append((os.path.splitext(fname)[0], f"{root}/{fname}"))
     return sorted(uid_path, key=lambda x: x[0])
 
 
-def write_scp(out_path, set_list):
+def write_scp(root_dir: str, out_path: str, set_list: list) -> None:
     maybe_makedir(os.path.dirname(out_path))
     with open(out_path, "w") as f:
         uid_path = []
         for set in set_list:
-            uid_path += find_audios(f"{args.librispeech_dir}/{set}")
+            uid_path += find_audios(f"{root_dir}/{set}")
         for uid, path in uid_path:
             f.write(f"{uid} {path}\n")
 
 
-write_scp(f"{args.out_dir}/train/wav.scp", args.train_list)
-write_scp(f"{args.out_dir}/dev/wav.scp", args.dev_list)
-write_scp(f"{args.out_dir}/test/wav.scp", args.test_list)
+def process_librispeech(
+    root_dir: str,
+    output_dir: str = "./datasets/librispeech_np_fbank",
+    train_list: list = None,
+    dev_list: list = None,
+    test_list: list = None,
+) -> None:
+    """Generates .scp files for the Librispeech dataset
 
-print("generated wav scp")
+    Args:
+        output_dir: Where .scp files should be saved
+        train_list: Training sets to process
+        dev_list:   Development sets to process
+        test_list:  Test sets to process
+    """
+    # avoid mutable default args
+    if train_list is None:
+        train_list = ["train-clean-100"]
+    if train_list is None:
+        dev_list = ["dev-clean", "dev-other"]
+    if train_list is None:
+        test_list = ["test-clean", "dev-other"]
+
+    write_scp(root_dir, f"{output_dir}/train/wav.scp", train_list)
+    write_scp(root_dir, f"{output_dir}/dev/wav.scp", dev_list)
+    write_scp(root_dir, f"{output_dir}/test/wav.scp", test_list)
+
+    print("generated wav scp")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "librispeech_dir", type=str, help="LibriSpeech raw data directory"
+    )
+    parser.add_argument(
+        "--ftype",
+        type=str,
+        default="fbank",
+        choices=["fbank", "spec"],
+        help="feature type",
+    )
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        default="./datasets/librispeech_np_fbank",
+        help="output data directory",
+    )
+    parser.add_argument(
+        "--train_list",
+        type=str,
+        nargs="*",
+        default=["train-clean-100"],
+        help="train sets to include {train-clean-100, train-clean-360, train-other-500}",
+    )
+    parser.add_argument(
+        "--dev_list",
+        type=str,
+        nargs="*",
+        default=["dev-clean", "dev-other"],
+        help="dev sets to include {dev-clean, dev-other}",
+    )
+    parser.add_argument(
+        "--test_list",
+        type=str,
+        nargs="*",
+        default=["test-clean", "dev-other"],
+        help="test sets to include {test-clean, test-other}",
+    )
+    args = parser.parse_args()
+    print(args)
+
+    process_librispeech(args.out_dir, args.train_list, args.dev_list, args.test_list)
--- preprocess_timit.py
+++ preprocess_timit.py
@@ -4,74 +4,96 @@ from sphfile import SPHFile
 from .utils import maybe_makedir
 
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("timit_dir", type=str, help="TIMIT raw data directory")
-parser.add_argument(
-    "--ftype", type=str, default="fbank", choices=["fbank", "spec"], help="feature type"
-)
-parser.add_argument(
-    "--out_dir",
-    type=str,
-    default="./datasets/timit_np_fbank",
-    help="output data directory",
-)
-parser.add_argument(
-    "--dev_spk",
-    type=str,
-    default="./misc/timit_dev_spk.list",
-    help="path to list of dev set speakers",
-)
-parser.add_argument(
-    "--test_spk",
-    type=str,
-    default="./misc/timit_test_spk.list",
-    help="path to list of test set speakers",
-)
-args = parser.parse_args()
-print(args)
+def process_timit(
+    root_dir: str,
+    output_dir: str = "./datasets/timit_np_fbank",
+    dev_spk_path: str = "./misc/timit_dev_spk.list",
+    test_spk_path: str = "./misc/timit_test_spk.list",
+) -> None:
+    """Generates .scp files for the TIMIT dataset
 
-# retrieve partition
-with open(args.dev_spk) as f:
-    dt_spks = [line.rstrip().lower() for line in f]
-with open(args.test_spk) as f:
-    tt_spks = [line.rstrip().lower() for line in f]
+    Args:
+        root_dir:      Directory holding the dataset
+        output_dir:    Where .scp files should be saved
+        dev_spk_path:  Path to a file containing all the speakers for the dev set
+        test_spk_path: Path to a file containing all the speakers for the train set
+    """
+    # retrieve partition
+    with open(dev_spk_path) as f:
+        dt_spks = [line.rstrip().lower() for line in f]
+    with open(test_spk_path) as f:
+        tt_spks = [line.rstrip().lower() for line in f]
 
-# convert sph to wav and dump scp
-wav_dir = os.path.abspath(f"{args.out_dir}/wav")
-tr_scp = f"{args.out_dir}/train/wav.scp"
-dt_scp = f"{args.out_dir}/dev/wav.scp"
-tt_scp = f"{args.out_dir}/test/wav.scp"
+    # convert sph to wav and dump scp
+    wav_dir = os.path.abspath(f"{output_dir}/wav")
+    tr_scp = f"{output_dir}/train/wav.scp"
+    dt_scp = f"{output_dir}/dev/wav.scp"
+    tt_scp = f"{output_dir}/test/wav.scp"
 
-maybe_makedir(wav_dir)
-maybe_makedir(os.path.dirname(tr_scp))
-maybe_makedir(os.path.dirname(dt_scp))
-maybe_makedir(os.path.dirname(tt_scp))
+    for file in (wav_dir, tr_scp, dt_scp, tt_scp):
+        maybe_makedir(file)
 
-tr_f = open(tr_scp, "w")
-dt_f = open(dt_scp, "w")
-tt_f = open(tt_scp, "w")
+    tr_f = open(tr_scp, "w")
+    dt_f = open(dt_scp, "w")
+    tt_f = open(tt_scp, "w")
 
-paths = []
-for root, _, fnames in sorted(os.walk(args.timit_dir)):
-    spk = root.split("/")[-1].lower()
-    if spk in dt_spks:
-        f = dt_f
-    elif spk in tt_spks:
-        f = tt_f
-    else:
-        f = tr_f
+    for root, _, fnames in sorted(os.walk(root_dir)):
+        spk = root.split("/")[-1].lower()
+        if spk in dt_spks:
+            f = dt_f
+        elif spk in tt_spks:
+            f = tt_f
+        else:
+            f = tr_f
 
-    for fname in fnames:
-        if fname.endswith(".wav") or fname.endswith(".WAV"):
-            sph_path = f"{root}/{fname}"
-            path = f"{wav_dir}/{spk}_{fname}"
-            uttid = f"{spk}_{os.path.splitext(fname)[0]}"
-            f.write(f"{uttid} {path}\n")
-            sph = SPHFile(sph_path)
-            sph.write_wav(path)
+        for fname in fnames:
+            if fname.endswith(".wav") or fname.endswith(".WAV"):
+                # WAV files are SPHERE-headered
+                sph_path = f"{root}/{fname}"
+                path = f"{wav_dir}/{spk}_{fname}"
+                uttid = f"{spk}_{os.path.splitext(fname)[0]}"
+                f.write(f"{uttid} {path}\n")
+                sph = SPHFile(sph_path)
+                sph.write_wav(path)
 
-tr_f.close()
-dt_f.close()
-tt_f.close()
+    tr_f.close()
+    dt_f.close()
+    tt_f.close()
 
-print("converted to wav and dumped scp files")
+    print("Converted to wav and dumped .scp files")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("timit_dir", type=str, help="TIMIT raw data directory")
+    parser.add_argument(
+        "--ftype",
+        type=str,
+        default="fbank",
+        choices=["fbank", "spec"],
+        help="feature type",
+    )
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        default="./datasets/timit_np_fbank",
+        help="output data directory",
+    )
+    parser.add_argument(
+        "--dev_spk",
+        type=str,
+        default="./misc/timit_dev_spk.list",
+        help="path to list of dev set speakers",
+    )
+    parser.add_argument(
+        "--test_spk",
+        type=str,
+        default="./misc/timit_test_spk.list",
+        help="path to list of test set speakers",
+    )
+    args = parser.parse_args()
+    print(args)
+
+    process_timit(args.timit_dir, args.out_dir, args.dev_spk, args.test_spk)
