25413779ad09f553077977e25e2d9499706a3909
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2020-03-25 05:51:59 -0400

    Refactor Kaldi data preprocessing

--- prepare_kaldi_data.py
+++ prepare_kaldi_data.py
@@ -4,15 +4,21 @@ import subprocess
 
 
 def prepare_kaldi(
-    dataset,
-    wav_scp,
-    feat_ark,
-    feat_scp,
-    len_scp,
+    dataset: str,
+    wav_scp: str,
+    feat_ark: str = None,
+    feat_scp: str = None,
+    len_scp: str = None,
     fbank_conf="./misc/fbank.conf",
     kaldi_root="./kaldi",
 ) -> None:
-    for p in [feat_ark, feat_scp, len_scp]:
+    out_files = [feat_ark, feat_scp, len_scp]
+    filenames = ("feats.ark", "feats.scp", "len.scp")
+
+    # If these names are not provided, save them to the same location as the wav.scp
+    for p, name in zip(out_files, filenames):
+        if p is None:
+            p = os.path.join(os.path.dirname(wav_scp), name)
         os.makedirs(os.path.dirname(p), exist_ok=True)
 
     feat_comp_cmd = [
@@ -23,6 +29,9 @@ def prepare_kaldi(
     ]
     feat_compute = subprocess.Popen(feat_comp_cmd)
 
+    # The next operation requires completion of this first command
+    feat_compute.wait()
+
     feat_len_cmd = [
         os.path.join(kaldi_root, "src/featbin/feat-to-len"),
         f"scp:{feat_scp}",
@@ -30,7 +39,9 @@ def prepare_kaldi(
     ]
     feat_to_len = subprocess.Popen(feat_len_cmd)
 
-    exit_codes = [p.wait() for p in (feat_compute, feat_to_len)]
+    feat_to_len.wait()
+
+    exit_codes = [p.returncode for p in (feat_compute, feat_to_len)]
     if any(exit_codes):
         commands = (" ".join(feat_comp_cmd), " ".join(feat_len_cmd))
         error_info = [
@@ -44,9 +55,13 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("wav_scp", type=str, help="Input wav scp file")
-    parser.add_argument("feat_ark", type=str, help="Output feats.ark file")
-    parser.add_argument("feat_scp", type=str, help="Output feats.scp file")
-    parser.add_argument("len_scp", type=str, help="Output len.scp file")
+    parser.add_argument(
+        "feat_ark", type=str, default=None, help="Output feats.ark file"
+    )
+    parser.add_argument(
+        "feat_scp", type=str, default=None, help="Output feats.scp file"
+    )
+    parser.add_argument("len_scp", type=str, default=None, help="Output len.scp file")
     parser.add_argument(
         "--dataset",
         type=str,
