9557d758551600b32d787f7c7be4998a4736e686
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2020-03-25 04:10:10 -0400

    Refactor numpy data preparation

--- prepare_numpy_data.py
+++ prepare_numpy_data.py
@@ -7,7 +7,7 @@ import time
 import contextlib
 from multiprocessing import Pool
 from nptyping import Array
-from typing import Dict
+from pathlib import Path
 
 
 def generate_feat(
@@ -49,55 +49,59 @@ def generate_feat(
     return feat
 
 
-def save_feats(
+def prepare_numpy(
     dataset: str,
     set_name: str,
-    ftype: str,
-    sample_rate: int,
-    win_t: float,
-    hop_t: float,
-    n_mels: int,
-    paths: Dict[str, str] = None,
+    ftype: str = "fbank",
+    sample_rate: int = None,
+    win_t: float = 0.025,
+    hop_t: float = 0.010,
+    n_mels: int = 80,
+    output_dir: str = None,
+    wav_scp: str = None,
+    feat_scp: str = None,
+    len_scp: str = None,
 ) -> int:
-    """Saves the generated features and script files for a dataset
+    """Handles feature and script file generation and saving
 
     Args:
-        dataset:     Name of the dataset (i.e. librispeech) being used
+        dataset:     Name of the dataset for which features are generated
         set_name:    Name of the set (train, dev, test) to operate on
         ftype:       Type of computed feature
         sample_rate: Sample rate for resampling if not None
         win_t:       FFT window size in seconds
         hop_t:       Frame spacing in seconds
         n_mels:      Number of filter banks if using 'fbank' as the computed feature
-        paths:       Dictionary of (optional) paths for saving files
-
-    Returns:
-        Number of files that were processed
+        output_dir:
+        wav_scp:
+        feat_scp:
+        len_scp:
 
     """
-    if paths is None:
-        paths = {}
+    opt_paths = (output_dir, wav_scp, feat_scp, len_scp)
 
-    root_dir = os.path.abspath(f"./datasets/{dataset}")
-    if paths.get("output_dir") is not None:
-        set_path = paths.get("output_dir")
+    root_dir = Path(os.path.abspath(f"./datasets/{dataset}"))
+    if output_dir is not None:
+        set_path = Path(output_dir)
     else:
-        set_path = os.path.join(root_dir, set_name)
-    if paths.get("wav_scp") is not None:
-        wav_path = paths.get("wav_scp")
-    else:
-        wav_path = os.path.join(set_path, "wav.scp")
-    if paths.get("feat_scp") is not None:
-        feat_path = paths.get("feat_scp")
-    else:
-        feat_path = os.path.join(set_path, "feats.scp")
-    if paths.get("len_scp") is not None:
-        len_path = paths.get("len_scp")
-    else:
-        len_path = os.path.join(set_path, "len.scp")
+        set_path = root_dir / set_name
 
-    set_start_time = time.time()
+    file_paths = []
+    for file, name in zip(opt_paths[1:], ("wav.scp", "feats.scp", "len.scp")):
+        if file is not None:
+            file_paths.append(Path(file))
+        else:
+            file_paths.append(set_path / name)
+
+    for p in file_paths:
+        os.makedirs(p, exist_ok=True)
+
+    wav_path, feat_path, len_path = file_paths
+
+    start_time = time.time()
     count = 0
+
+    # Opening multiple files at once with context manager
     with contextlib.ExitStack() as stack:
         wavfile = stack.enter_context(open(wav_path))
         featfile, lenfile = [
@@ -119,52 +123,12 @@ def save_feats(
             lenfile.write(f"{seq} {len(feat)}\n")
             count = i + 1
             if (count) % 1000 == 0:
-                print(
-                    f"{count} files in {set_name} set over {time.time() - set_start_time} seconds."
-                )
-    return count
-
-
-def prepare_numpy(
-    dataset: str,
-    ftype: str = "fbank",
-    sample_rate: int = None,
-    win_t: float = 0.025,
-    hop_t: float = 0.010,
-    n_mels: int = 80,
-    **kwargs,
-) -> None:
-    """Handles feature and script file generation and saving
-
-    Args:
-        dataset:     Name of the dataset for which features are generated
-        ftype:       Type of computed feature
-        sample_rate: Sample rate for resampling if not None
-        win_t:       FFT window size in seconds
-        hop_t:       Frame spacing in seconds
-        n_mels:      Number of filter banks if using 'fbank' as the computed feature
-
-    """
-    paths = {}
-    for pathvar in ["wave_scp", "output_dir", "feat_scp", "len_scp"]:
-        if pathvar in kwargs:
-            paths[pathvar] = kwargs[pathvar]
-
-    for path in paths:
-        os.makedirs(paths[path], exist_ok=True)
-
-    starmap_args = []
-    for s in ["train", "dev", "test"]:
-        func_args = [dataset, s, ftype, sample_rate, win_t, hop_t, n_mels, paths]
-        starmap_args.append(tuple(func_args))
-
-    files_start_time = time.time()
-    with Pool(3) as p:
-        results = p.starmap(save_feats, starmap_args)
+                print(f"{count} files in {time.time() - start_time} seconds.")
 
     print(
-        f"Processed {sum(results)} files in {time.time() - files_start_time} seconds."
+        f"Processed {count} files in {set_name} set over {time.time() - start_time} seconds."
     )
+    return count
 
 
 if __name__ == "__main__":
@@ -181,6 +145,12 @@ if __name__ == "__main__":
         default="librispech",
         choices=["librispeech", "timit"],
         help="Dataset name",
+    )
+    parser.add_argument(
+        "--set_name",
+        type=str,
+        default=None,
+        help="Set {train, dev, test} to operate on, Leave blank for all three",
     )
     parser.add_argument(
         "--ftype",
@@ -210,15 +180,32 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
 
-    prepare_numpy(
+    func_args = [
         args.dataset,
+        args.set_name,
         args.ftype,
         args.sr,
         args.win_t,
         args.hop_t,
         args.n_mels,
-        wav_scp=args.wav_scp,
-        feat_scp=args.feat_scp,
-        len_scp=args.len_scp,
-        output_dir=args.np_dir,
-    )
+        args.np_dir,
+        args.wav_scp,
+        args.feat_scp,
+        args.len_scp,
+    ]
+    # Parallel run if set_name is unspecified
+    if args.set_name is None:
+        starmap_args = []
+        for s in ["train", "dev", "test"]:
+            func_args[1] = s
+            starmap_args.append(tuple(func_args))
+
+        files_start_time = time.time()
+        with Pool(3) as p:
+            results = p.starmap(prepare_numpy, starmap_args)
+
+        print(
+            f"Processed {sum(results)} files in {time.time() - files_start_time} seconds."
+        )
+    else:
+        prepare_numpy(*func_args)
